1731d7e690111a7a63ac8460df2bc1a1ad35839c
soorim-coding-kr <soorim.coding.kr@gmail.com>
2021-06-10 09:53:06 +0900

    디렉토리 변경 및 jsx수업내용 추가, react 수업 내용 추가

---
+++ 210609_react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Test2 from './components/Test2';
+// import Test3 from './components/Test3';
+import Test4 from './components/Test4';
+
+const App = () => {
+  return (
+    <div>
+      {/* <Test2/> */}
+      {/* <Test3/> */}
+      <Test4/>
+    </div>
+  );
+};
+
+export default App;
