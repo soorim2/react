2c61c2d62b1b96b402abf82763f8a5917b5bb140
soorim <soorim@starlabs.co.kr>
2021-06-15 11:06:40 +0900

    복습(map, 화살표함수, useState,  삼항연산자)

--- 210614_react/src/App.js
+++ 210614_react/src/App.js
@@ -1,12 +1,11 @@
-import Test8 from "./components/Test8";
+import Test8 from './components/Test8';
 
 function App() {
-  return (
-    <>
-      <Test8/>
-      
-    </>
-  );
+	return (
+		<>
+			<Test8 />
+		</>
+	);
 }
 
 export default App;
