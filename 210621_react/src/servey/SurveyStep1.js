85d674f13fbc2350e8eea857f95033e4f270bd78
soorim-coding-kr <soorim.coding.kr@gmail.com>
2021-06-21 23:09:19 +0900

    210621_react 컴포넌트 분리 데이터 전달

---
+++ 210621_react/src/servey/SurveyStep1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SurveyStep1 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SurveyStep1;
---
+++ 210621_react/src/servey/SurveyStep3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SurveyStep3 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SurveyStep3;
