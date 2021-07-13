d54cb56194825c6047cd4cc6bdbd6d440766a01c
soorim <soorim@starlabs.co.kr>
2021-07-13 18:12:07 +0900

    source update

---
+++ 210712_react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import './assets/css/reset.css' 
+import Melon from './melon/Melon'
+
+const App = () => {
+  return (
+    <div>         
+      <Melon />
+    </div>
+  );
+};
+
+export default App;
---
+++ 210712_react/src/melon/MelonList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import MelonItem from './MelonItem';
+import './MelonList.css'
+
+const MelonList = ({musics}) => {
+    return (
+        <div className="MelonList">
+            <table>
+                <colgroup>
+                    <col className="w1" />
+                    <col className="w2" />
+                    <col className="w3" />
+                    <col className="w4" />
+                    <col className="w5" />
+                    <col className="w6" />
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th>순위</th>
+                        <th>곡(가수)</th>
+                        <th>앨범</th>
+                        <th>좋아요</th>
+                        <th>동영상</th>
+                        <th>상승/하락</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        musics.map( music => <MelonItem key={music.id} music={music} />)
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default MelonList;
