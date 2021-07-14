095b8c8016c3718e1858e392fb2969b8052c114b
soorim <soorim@starlabs.co.kr>
2021-07-14 13:25:51 +0900

    검색 및 동영상 임배드

--- 210709_react/src/melon/Modal.js
+++ 210709_react/src/melon/Modal.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './Modal.css'
 
-const Modal = () => {
+const Modal = ({musicNum,onClose}) => {
+    const {title,key,singer} = musicNum
     return (
         <div className="Modal">
-            
+            <div className="bg" ></div>            
+            <div className="popup">
+            <h2>제목: {title} </h2>
+                <div>
+                <iframe title="video" src={`https://www.youtube.com/embed/${key}`} frameBorder="0"></iframe>
+                {/* <iframe title="video" src={`https://www.youtube.com/embed/${key}`} frameBorder="0"></iframe> */}
+                </div>
+                <p>가수 :{singer} </p>
+                <span >
+                    <i onClick={onClose}> X </i>
+                </span>
+         
+            </div>
+            {/* <div className="bg" onClick={onClose}></div>            
+            <div className="popup">
+                <h2>제목: {title} </h2>
+                <div>
+                <iframe  src={`https://www.youtube.com/embed/${key}`}frameborder="0"  ></iframe>
+                </div>
+                <p>가수 :{singer} </p>
+                <span onClick={onClose}>
+                    <i> X </i>
+                </span>
+            </div> */}
         </div>
     );
 };
