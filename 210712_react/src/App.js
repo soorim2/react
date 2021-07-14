495998181cff678c9e55958771b7bccc7fd60aec
soorim <soorim@starlabs.co.kr>
2021-07-14 14:44:41 +0900

    bootstrap 적용

--- 210712_react/src/App.js
+++ 210712_react/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react'; 
-import './assets/css/reset.css' 
-import Melon from './melon/Melon'
+import React from 'react';
+import './assets/css/reset.css';
+// import Melon from './melon/Melon'
+import Test1 from './components/Test1';
 
 const App = () => {
-  return (
-    <div>         
-      <Melon />
-    </div>
-  );
+	return (
+		<div>
+			{/* <Melon /> */}
+			<Test1 />
+		</div>
+	);
 };
 
 export default App;
