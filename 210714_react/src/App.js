71eb43ad14a8c2ced2d612d7707e4683697932fd
soorim-coding-kr <soorim.coding.kr@gmail.com>
2021-07-14 20:34:58 +0900

    sass 응용

---
+++ 210714_react/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Test2 from './components/Test2';
+///// module.css 예시
+// import Header1 from './project1/Header';
+// import Header2 from './project2/Header';
+// import Content1 from './project1/Content1';
+// import Content2 from './project2/Content1';
+
+const App = () => {
+  return (
+    <div>
+        {/* module.css 예시 */}
+        {/* <Header1/>
+        <Header2/>
+        <Content1/>
+        <Content2/> */}
+        <Test2/>
+    </div>
+  );
+};
+
+export default App;
---
+++ 210714_react/src/project2/Content1.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './Content1.module.css';
+
+const Content1 = () => {
+    return (
+        <div className={style.conBox}>
+            <article>
+                <h3>test1</h3>
+            </article>
+            <article>
+                <h3>test1</h3>
+            </article>
+            <article>
+                <h3>test1</h3>
+            </article>
+            <article>
+                <h3>test1</h3>
+            </article>
+        </div>
+    );
+};
+
+export default Content1;
